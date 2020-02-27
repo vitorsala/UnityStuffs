07aab357ab2a20353d7a38714b5fcda832a806fc
vitorsala <vitorsala@gmail.com>
2020-02-26 23:40:03 -0300

    clock

---
+++ Assets/Clock/Scripts/Clock.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Clock : MonoBehaviour
+{
+
+    public ClockIndicator hours, minutes, seconds;
+    public bool continuous;
+
+    /// <summary>
+    /// Awake is called when the script instance is being loaded.
+    /// </summary>
+    void Awake()
+    {
+        UpdateBars();
+    }
+
+    /// <summary>
+    /// Update is called every frame, if the MonoBehaviour is enabled.
+    /// </summary>
+    void Update()
+    {
+        UpdateBars();
+    }
+
+    void UpdateBars()
+    {
+        if (continuous)
+        {
+            UpdateBarsContinuous();
+        }
+        else
+        {
+            UpdateBarsDiscrete();
+        }
+    }
+
+    void UpdateBarsDiscrete()
+    {
+        DateTime currentTime = DateTime.Now;
+        hours.UpdateRotation(currentTime.Hour);
+        minutes.UpdateRotation(currentTime.Minute);
+        seconds.UpdateRotation(currentTime.Second);
+    }
+
+    void UpdateBarsContinuous()
+    {
+        TimeSpan currentTime = DateTime.Now.TimeOfDay;
+        hours.UpdateRotation((float)currentTime.TotalHours);
+        minutes.UpdateRotation((float)currentTime.TotalMinutes);
+        seconds.UpdateRotation((float)currentTime.TotalSeconds);
+    }
+}
---
+++ Assets/Clock/Scripts/ClockIndicator.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ClockIndicator : MonoBehaviour
+{
+    public enum TimeComponentType
+    {
+        hours,
+        minutes,
+        seconds
+    }
+
+    public TimeComponentType type;
+
+    private const float degreesPerHour = 30f;
+    private const float degreesPerMinute = 6f;
+    private const float degreesPerSecond = 6f;
+
+    public void UpdateRotation(float timeComponent)
+    {
+        float rotation = 0;
+        switch (this.type)
+        {
+            case TimeComponentType.hours:
+                rotation = Degree(timeComponent);
+                break;
+            case TimeComponentType.minutes:
+                rotation = Degree(timeComponent);
+                break;
+            case TimeComponentType.seconds:
+                rotation = Degree(timeComponent);
+                break;
+        }
+        this.transform.localRotation = Quaternion.Euler(0f, rotation, 0f);
+    }
+    private float Degree(float timeComponent)
+    {
+        float degreesPerComponent = 0f;
+        switch (this.type)
+        {
+            case TimeComponentType.hours:
+                degreesPerComponent = degreesPerHour;
+                break;
+            case TimeComponentType.minutes:
+                degreesPerComponent = degreesPerMinute;
+                break;
+            case TimeComponentType.seconds:
+                degreesPerComponent = degreesPerSecond;
+                break;
+        }
+        return timeComponent * degreesPerComponent;
+    }
+}
